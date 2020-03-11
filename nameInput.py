c04c3911a874d43202e0362cb8a8b25a3ea78053
jhon-swai <jhonswia@gmail.com>
2020-03-11 12:41:20 +0300

    taking input from user

---
+++ nameInput.py
@@ -0,0 +1,10 @@
+while True:
+    print('what is your name', sep=': ')
+    name = input()
+    if( name == 'chinaa'):
+        print('Welcome back programmer')
+        break
+    else:
+        print('wrong input')
+    
+    
